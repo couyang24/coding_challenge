be683a7a54ded0e565f3dcd5ede4e72b3c554754
Chengran Ouyang <30837746+couyang24@users.noreply.github.com>
2019-01-27 22:26:01 -0500

    update

--- TwoSum/main.py
+++ TwoSum/main.py
@@ -5,3 +5,4 @@ for inputIndex1 in range(len(nums)):
     for inputIndex2 in range(len(nums)-(inputIndex1+1)):
         if nums[inputIndex1]+nums[inputIndex2+inputIndex1+1] == target:
             print([inputIndex1, inputIndex2+inputIndex1+1])
+
