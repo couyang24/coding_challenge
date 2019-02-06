5bbd25c70b27e2a410e846e0e85eefc374a8f8a4
Chengran Ouyang <30837746+couyang24@users.noreply.github.com>
2019-02-05 19:38:33 -0500

    uniqueemail2

--- UniqueEmail/main.py
+++ UniqueEmail/main.py
@@ -10,6 +10,16 @@ domain
 if '+' in local:
     local = local[:local.index('+')].replace('.','')
 
+    uniqueEmail.add(local + '@' + domain)
 
 
-uniqueEmail.add(local+'@'+domain)
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        uniqueEmail = set()
+        for email in emails:
+            local, domain = email.split('@')
+            if '+' in local:
+                local = local[:local.index('+')]
+            uniqueEmail.add(local.replace('.', '') + '@' + domain)
+        return len(uniqueEmail)
+
