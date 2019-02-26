556199195ce2b267b6e657873f85e2d7bf1a7172
couyang24 <30837746+couyang24@users.noreply.github.com>
2019-02-26 11:35:39 -0500

    numCompliment

--- numberCompliment/main.py
+++ numberCompliment/main.py
@@ -1,13 +1,14 @@
-num = 5
+num = 1
 
 num % 2
 5//2
 
 count = 0
-
+power = 0
 while num != 0:
-    if num % 2 == 1:
-        count += 1
+    if num % 2 == 0:
+        count += 2 ** power
+    power += 1
     num = num // 2
 
 print(count)
@@ -20,10 +21,10 @@ print(count)
 class Solution:
     def findComplement(self, num: int) -> int:
         count = 0
-
-        while num // 2 != 0:
-            if num % 2 == 1:
-                count += 1
-            num = num / 2
-
+        power = 0
+        while num != 0:
+            if num % 2 == 0:
+                count += 2 ** power
+            power += 1
+            num = num // 2
         return count
