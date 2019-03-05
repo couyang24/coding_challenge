5a5e0af3074babde0de52f6109feb9c383003191
couyang24 <30837746+couyang24@users.noreply.github.com>
2019-03-05 12:52:31 -0500

    findMissingNotSubmittedc

--- findMissing/main.py
+++ findMissing/main.py
@@ -1,5 +1,7 @@
 nums = [4,3,2,7,8,2,3,1]
 nums = [1,1]
+nums = [2,2]
+
 max(nums)
 min(nums)
 
@@ -11,29 +13,31 @@ if nums == []:
 
 
 ans = []
-for num in range(min(nums), len(nums)+1):
+for num in range(1, len(nums)+1):
     if num not in nums:
         ans.append(num)
 
 print(ans)
 
 
+def findDisappearedNumbers(nums):
+    if nums == []:
+        return []
+    ans = []
+    for num in range(1, len(nums) + 1):
+        if num not in nums:
+            ans.append(num)
+    return ans
 
-
-
-
-
-
-
-
-
-
+findDisappearedNumbers(nums)
 
 
 class Solution:
-    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+    def findDisappearedNumbers(self, nums):
+        if nums == []:
+            return []
         ans = []
-        for num in range(min(nums), max(nums) + 1):
+        for num in range(1, len(nums) + 1):
             if num not in nums:
                 ans.append(num)
         return ans
