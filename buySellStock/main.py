dfa5e5716453aa409e7b03818d711da94d591c28
couyang24 <30837746+couyang24@users.noreply.github.com>
2019-05-30 10:27:20 -0400

    update

--- buySellStock/main.py
+++ buySellStock/main.py
@@ -17,6 +17,24 @@ for index in range(len(prices)):
 print(profit)
 
 
+def maxProfit(prices):
+    max_profit, min_price = 0, float('inf')
+    for price in prices:
+        min_price = min(min_price, price)
+        profit = price - min_price
+        max_profit = max(max_profit, profit)
+    return max_profit
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+        minPrice = float('inf')
+        for p in prices:
+            minPrice = min(minPrice, p)
+            maxProfit = max(maxProfit, p - minPrice)
+        return maxProfit
+
+
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
